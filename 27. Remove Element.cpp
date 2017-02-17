37b9e54d4c3468cfd407eaf9e927dff9019fda8b
chenlin2844 <907169968@qq.com>
2017-02-17 22:02:13 +0800

    Remove Element

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -2,13 +2,27 @@
 * @Author: Administrator
 * @Date:   2017-02-17 21:45:27
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-02-17 21:46:40
-* @Ps: Thank daxianji007's share
+* @Last Modified time: 2017-02-17 22:01:39
+* @score: solution1:54.56%;solution2:10.21%
+* @Ps: solution2 by daxianji007's share
 */
 
 class Solution {
 public:
 
+	int removeElement(vector<int>& nums, int val) {
+		int last = nums.size();
+		int i = 0;
+		while(i<last){
+			if (nums[i] == val){
+				nums[i] = nums[--last];
+			}else{
+				i++;
+			}
+		}
+		return last;
+	}
+    /*
     int removeElement(vector<int>& nums, int val) {
         int flag = 0;
         for (int i= 0;i<nums.size();i++)
@@ -19,5 +33,6 @@ public:
         }
         return flag;
     }
-    
+    */
+	
 };
